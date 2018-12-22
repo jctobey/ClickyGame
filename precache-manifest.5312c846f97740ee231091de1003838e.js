5c1c57f5071502ff94f98753ca2a1f2e6fe7bee9
James Tobey <jctobey@eckerd.edu>
2018-12-22 14:13:36 -0600

    Updates

---
+++ precache-manifest.5312c846f97740ee231091de1003838e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "8002e47676e227ad755e",
+    "url": "/clickygame/static/js/main.8002e476.chunk.js"
+  },
+  {
+    "revision": "fb9f72c6d9bfc22bcecb",
+    "url": "/clickygame/static/js/1.fb9f72c6.chunk.js"
+  },
+  {
+    "revision": "8002e47676e227ad755e",
+    "url": "/clickygame/static/css/main.d03208ac.chunk.css"
+  },
+  {
+    "revision": "8a0ecada8e1704cb8c8ad4b9f84ef736",
+    "url": "/clickygame/index.html"
+  }
+];
